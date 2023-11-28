02ac8cf78231b090a61f043fe24eabde685a3a0a
cristian1016 <cpulgarinsalgado@gmail.com>
2023-11-28 08:27:19 -0500

    add desing footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -6,7 +6,24 @@ export default function Footer() {
   return (
     <>
     <footer>
-        <p>footer</p>
+      <div>footer</div>
+        {/* <div>
+          <img src="/tienda_campe.png" alt="#" />
+          <h3>Sobre nosotros</h3>
+          <p>Colombiana de Comercio S.A.</p>
+          <p>NIT: 890.900.943-1</p>
+        </div>
+
+        <div>
+          <img src="/email_campe.png" alt="#" />
+          <p>mercadocampesino@gmail.com</p>
+        </div>
+
+        <div>
+          <img src="/telefono_campe.png" alt="#" />
+          <p>+57 313 629 7064</p>
+          <p>+57 214 567 2345</p>
+        </div> */}
     </footer>
     </>
   )
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -38,6 +38,8 @@ export default function Header() {
                     </div> */}
                 </div>
 
+                
+
             </header>
         </>
     )
